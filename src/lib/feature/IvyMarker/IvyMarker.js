5ab676d45826cd6403c0452ae524fcbdfb693198
oustriem <marco.buri@gmail.com>
2015-08-24 10:39:37 +0200

    bpmn-js update, executed element id's defined

--- src/lib/feature/IvyMarker/IvyMarker.js
+++ src/lib/feature/IvyMarker/IvyMarker.js
@@ -4,11 +4,7 @@ var MARKER_EXECUTED_ELEMENT = 'executed-element',
     MARKER_EXECUTED_ARROW = 'executed-arrow';
 
 var executedElementIds = [
-    'sid-D7F237E8-56D0-4283-A3CE-4F0EFE446138', // start
-    'sid-7B791A11-2F2E-4D80-AFB3-91A02CF2B4FD', // arrow
-    'sid-5134932A-1863-4FFA-BB3C-A4B4078B11A9', // exclusive gateway
-    'sid-4DC479E5-5C20-4948-BCFC-9EC5E2F66D8D', // arrow
-    'sid-52EB1772-F36E-433E-8F5B-D5DFD26E6F26'  // task 'Scan QR code'
+    'START', 'FLOW_START_TO_MERGE', 'MERGE', 'FLOW_MERGE_TO_SCAN', 'SCAN_QR_CODE', 'FLOW_SCAN_TO_SCAN_OK', 'SCAN_OK', 'FLOW_SCAN_OK_TO_MERGE'
 ];
 
 function IvyMarker(eventBus, canvas, elementRegistry) {
