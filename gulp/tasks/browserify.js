6170ad583101bd8cad487211868958fcc22a7576
oustriem <marco.buri@gmail.com>
2015-08-31 11:01:43 +0200

    merging before pull request

--- gulp/tasks/browserify.js
+++ gulp/tasks/browserify.js
@@ -5,44 +5,34 @@ var gulp = require('gulp');
 var source = require('vinyl-source-stream');
 var buffer = require('vinyl-buffer');
 //var uglify = require('gulp-uglify');
+var watchify = require('watchify');
 var sourcemaps = require('gulp-sourcemaps');
 var gutil = require('gulp-util');
 
 var config = require('../config');
 
-var browserifyTask = function() {
 
-    var b = browserify({
-        entries: config.app.start,
-        debug: true // include source maps.
-    });
-    b.transform('brfs');
-    var bundle = function() {
-        return b.bundle()
-            .pipe(source('showcases.js'))
-            .pipe(buffer())
-            .pipe(sourcemaps.init({loadMaps: true}))
-            //.pipe(uglify())
-            .on('error', gutil.log)
-            .pipe(sourcemaps.write('./'))
-            .pipe(gulp.dest(config.app.dist));
-    };
-
-    /*
-    if(devMode) {
-        // Wrap with watchify and rebundle on changes
-        b = watchify(b);
-        // Rebundle on update
-        b.on('update', bundle);
-        bundleLogger.watch(bundleConfig.outputName);
-    }
-    */
-
-    return bundle();
-}
-
-gulp.task('browserify', function() {
-    return browserifyTask()
+var b = browserify({
+    entries: config.app.start,
+    debug: true // include source maps.
 });
+b.transform('brfs');
 
-module.exports = browserifyTask;
+b = watchify(b);
+
+var bundle = function() {
+    return b.bundle()
+        .pipe(source('showcases.js'))
+        .pipe(buffer())
+        .pipe(sourcemaps.init({loadMaps: true}))
+        //.pipe(uglify())
+        .on('error', gutil.log)
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest(config.app.dist));
+};
+
+gulp.task('browserify', bundle);
+
+b.on('update', bundle); // on any dep update, runs the bundler
+b.on('log', gutil.log); // output build logs to terminal
+
