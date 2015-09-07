7467d31010c8b0d28df4ca76ce32bf296e4854ac
oustriem <marco.buri@gmail.com>
2015-09-07 14:24:15 +0200

    karma/jasmine/phantomjs basic setup

--- gulp/tasks/test.js
+++ gulp/tasks/test.js
@@ -1,16 +1,37 @@
 var gulp = require('gulp');
-var Jasmine = require('jasmine');
-var jasmine = new Jasmine();
+var Server = require('karma').Server;
 
-gulp.task('test', function () {
-    jasmine.loadConfigFile('./test/spec/support/jasmine.json');
-    jasmine.onComplete(function (passed) {
-        if (passed) {
-            console.log('All specs have passed');
-        }
-        else {
-            console.log('At least one spec has failed');
-        }
-    });
-    jasmine.execute();
+/**
+ * Run test once and exit
+ */
+gulp.task('test', function (done) {
+    new Server({
+        configFile: __dirname + '/../../test/karma.conf.js',
+        singleRun: true
+    }, done).start();
 });
+
+//var Jasmine = require('jasmine');
+//var jasmine = new Jasmine();
+//var jasmine = require('gulp-jasmine-phantom');
+
+//gulp.task('jasmine', function () {
+//    jasmine.loadConfigFile('./test/spec/support/jasmine.json');
+//    jasmine.onComplete(function (passed) {
+//        if (passed) {
+//            console.log('All specs have passed');
+//        }
+//        else {
+//            console.log('At least one spec has failed');
+//        }
+//    });
+//    jasmine.execute();
+//});
+
+//gulp.task('jasmine phantom', function() {
+//    return gulp.src('./test/**/*[sS]pec.js')
+//        .pipe(jasmine({
+//            integration: true,
+//            keepRunner: './'
+//        }));
+//});
--- test/spec/IvyMarker/IvyMarkerSpec.js
+++ test/spec/IvyMarker/IvyMarkerSpec.js
@@ -1,22 +1,23 @@
 'use strict';
 
-var log4js = require('log4js');
-var logger = log4js.getLogger();
 var IvyMarker = require('../../../src/lib/feature/IvyMarker');
 
 describe('IvyMarker test suite', function() {
 
     // Consider that a direct call won't work ... e.g.: beforeEach(setUp());
     beforeEach(function() {
-        setUp('IvyMarker test');
+        //setUp('IvyMarker test');
     });
 
     afterEach(function() {
-        tearDown('IvyMarker test');
+        //tearDown('IvyMarker test');
     });
 
     it("contains spec with an expectation", function() {
-        logger.debug('logging test' + IvyMarker);
+        expect(true).toBe(true);
+    });
+
+    it('second test case', function() {
         expect(true).toBe(true);
     });
 
